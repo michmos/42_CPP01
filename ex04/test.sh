ec0ebdc6c93c582b3b8604b8cf1fd1b1a72a97b5
Michael Moser <Michael.Moser11@web.de>
2024-10-18 14:17:38 +0200

    ex04 - update tester

--- ex04/test.sh
+++ ex04/test.sh
@@ -21,7 +21,7 @@ test() {
 
 	./cheapSed $file_name "${s1}" "${s2}"
 
-	diff -y $file_name "$file_name.replace"
+	diff -y --suppress-common-lines $file_name "$file_name.replace"
 }
 
 setup_file() {
@@ -41,31 +41,52 @@ make
 print_test_case "non existing file"
 FILENAME="non_existing.txt"; CREATE=0
 setup_file
-test "world" "welt"
+test "word" "___"
+
+print_test_case "empty file"
+FILENAME="empty_file.txt"; CREATE=1; CONTENT=""
+setup_file
+test "word" "___"
 
 print_test_case "one line without line break"
-FILENAME="one_line_without_lb.txt"; CREATE=1; CONTENT="hello world"
+FILENAME="one_line_without_lb.txt"; CREATE=1; CONTENT="this word"
 setup_file
-test "world" "welt"
+test "word" "___"
 
 print_test_case "one line with line break"
-FILENAME="one_line.txt"; CREATE=1; CONTENT="hello world\n"
+FILENAME="one_line.txt"; CREATE=1; CONTENT="this word\n"
 setup_file
-test "world" "welt"
+test "word" "___"
 
 print_test_case "replace inner word"
-FILENAME="replace_inner_word.txt"; CREATE=1; CONTENT="asdfworldadsf\n"
+FILENAME="replace_inner_word.txt"; CREATE=1; CONTENT="asdfwordadsf\n"
 setup_file
-test "world" "welt"
+test "word" "___"
 
 print_test_case "s1 is contained inside s2"
 FILENAME="s1_in_s2.txt"; CREATE=1; CONTENT="here is a test\n"
 setup_file
-test "is" "here is what has been inserted"
+test "is" "____is____"
 
 print_test_case "bigger file"
 FILENAME="big_text.txt";
 test "is" "__"
+
+print_test_case "empty s1"
+FILENAME="big_text.txt";
+test "" "hello"
+
+print_test_case "empty s2"
+FILENAME="big_text.txt";
+test "is" ""
+
+print_test_case "empty strings"
+FILENAME="big_text.txt";
+test "" ""
+
+print_test_case "same strings"
+FILENAME="big_text.txt";
+test "is" "is"
 
 print_test_case "even bigger file with garbage chars"
 FILENAME="weird_chars.txt";
